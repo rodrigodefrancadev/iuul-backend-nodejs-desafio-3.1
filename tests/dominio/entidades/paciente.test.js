a772d097ae5efb10e1b4d345983e1bd6777750b3
Rodrigo de França <rodrigodefrancadev@gmail.com>
2024-11-15 17:07:04 -0300

    testes da entidade Paciente

--- tests/dominio/entidades/paciente.test.js
+++ tests/dominio/entidades/paciente.test.js
@@ -2,16 +2,22 @@
 
 import { describe, it } from "node:test"
 import assert from "node:assert/strict"
-import { Paciente } from "../../../src/dominio/entidades/paciente";
-import { Cpf } from "../../../src/dominio/campos/cpf";
-import { NomeDePessoa } from "../../../src/dominio/campos/nome-de-pessoa";
-import { DataDeNascimento } from "../../../src/dominio/campos/data-de-nascimento";
+import { Paciente } from "../../../src/dominio/entidades/paciente.js";
+import { Cpf } from "../../../src/dominio/campos/cpf.js";
+import { NomeDePessoa } from "../../../src/dominio/campos/nome-de-pessoa.js";
+import { DataDeNascimento } from "../../../src/dominio/campos/data-de-nascimento.js";
+import { DateTime } from "luxon";
 
 describe("Entidade: Paciente", () => {
     it('deve criar um paciente com sucesso', () => {
+        const idade = 18;
         const cpf = new Cpf('22375922093');
         const nome = new NomeDePessoa('Fulano de Tal');
-        const dataDeNascimento = new DataDeNascimento(new Date('2000-01-01'));
-        assert.doesNotThrow(() => new Paciente(cpf, nome, dataDeNascimento))
+        const dataJsDeNascimento = DateTime.fromJSDate(new Date()).minus({ years: idade }).toJSDate();
+        const dataDeNascimento = new DataDeNascimento(dataJsDeNascimento);
+
+        const paciente = new Paciente(cpf, nome, dataDeNascimento);
+        assert.equal(paciente.idade, idade);
+
     })
 });
