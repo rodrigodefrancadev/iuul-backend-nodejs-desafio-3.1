91d644ffd63b64aae53eb3c5422f0a71d0d23774
Rodrigo de França <rodrigodefrancadev@gmail.com>
2024-11-15 17:17:31 -0300

    implementação da entidade agendamento

---
+++ src/dominio/entidades/agendamento.js
@@ -0,0 +1,50 @@
+//@ts-check
+
+import { Cpf } from "../campos/cpf.js";
+import { IntervaloDeHorario } from "../campos/intervalo-de-horario.js";
+
+export class Agendamento {
+
+    #cpfDoPaciente;
+    #dia;
+    #intervaloDeHorario;
+
+    get cpfDoPaciente() {
+        return this.#cpfDoPaciente
+    }
+
+    get dia() {
+        return this.#dia;
+    }
+
+    get intervaloDeHorario() {
+        return this.#intervaloDeHorario;
+    }
+
+    /**
+     * 
+     * @param {Cpf} cpfDoPaciente
+     * @param {Date} dia 
+     * @param {IntervaloDeHorario} intervaloDeHorario
+     */
+    constructor(cpfDoPaciente, dia, intervaloDeHorario) {
+        Agendamento.#validaIntervaloDeHorario(intervaloDeHorario);
+
+
+        this.#cpfDoPaciente = cpfDoPaciente;
+        this.#dia = dia;
+        this.#intervaloDeHorario = intervaloDeHorario;
+    }
+
+    /**
+     * @param {IntervaloDeHorario} intervaloDeHorario 
+    */
+    static #validaIntervaloDeHorario(intervaloDeHorario) {
+        const minutosValidos = [0, 15, 30, 45];
+        const minutoInicio = intervaloDeHorario.inicio.minuto;
+        const minutoFim = intervaloDeHorario.fim.minuto;
+        if (!minutosValidos.includes(minutoInicio) || !minutosValidos.includes(minutoFim)) {
+            throw new Error('O Intervalo de Horario do Agendamento deve ser definido em termos de 15 em 15 minutos');
+        }
+    }
+}
