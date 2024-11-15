fd9c04ad485ca7aa6465b1440c3dbda5aef9b522
Rodrigo de França <rodrigodefrancadev@gmail.com>
2024-11-15 16:12:11 -0300

    adicionado campo CPF

--- src/dominio/campos/cpf.js
+++ src/dominio/campos/cpf.js
@@ -20,7 +20,43 @@ export class Cpf {
      * @param {string} cpfStr
      */
     static #validar(cpfStr) {
-        throw new Error('Não implementado');
+        if (cpfStr.length < 11) {
+            throw new Error('CPF inválido');
+        }
+
+        const algarismos = Array(10).fill(null).map((_, i) => i.toString())
+        if (cpfStr.split('').some(caracter => !algarismos.includes(caracter))) {
+            throw new Error('CPF inválido');
+        }
+
+        const cpfsDeNumerosRepetidos = algarismos.map((i) => i.repeat(11));
+        if (cpfsDeNumerosRepetidos.includes(cpfStr)) {
+            throw new Error('CPF inválido');
+        }
+
+        const digitosDoCpf = cpfStr.split('').map(Number);
+
+        const digitoVerificador1Esperado = (() => {
+            const soma = digitosDoCpf.slice(0, 9).map((d, i) => d * (10 - i)).reduce((acc, x) => acc + x, 0);
+            const resto = soma % 11;
+            const digito1 = resto <= 1 ? 0 : (11 - resto);
+            return digito1;
+        })();
+
+        if (digitosDoCpf[9] !== digitoVerificador1Esperado) {
+            throw new Error('CPF inválido');
+        }
+
+        const digitoVerificador2Esperado = (() => {
+            const soma = digitosDoCpf.slice(0, 10).map((d, i) => d * (11 - i)).reduce((acc, x) => acc + x, 0);
+            const resto = soma % 11;
+            const digito2 = resto <= 1 ? 0 : (11 - resto);
+            return digito2;
+        })();
+
+        if (digitosDoCpf[10] !== digitoVerificador2Esperado) {
+            throw new Error('CPF inválido');
+        }
     }
 
     toString() {
@@ -29,5 +65,13 @@ export class Cpf {
 
     toJson() {
         return this.#valor;
+    }
+
+    /**
+     * 
+     * @param {Cpf} outroCpf 
+     */
+    equal(outroCpf) {
+        return this.#valor === outroCpf.#valor;
     }
 }
