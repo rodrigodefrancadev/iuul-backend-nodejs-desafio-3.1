64cb3da4ca9723350f1b1bc886849ed897255db2
Rodrigo de França <rodrigodefrancadev@gmail.com>
2024-12-12 14:08:08 -0300

    cancelar agendamento use case

--- src/dominio/entidades/agendamento.js
+++ src/dominio/entidades/agendamento.js
@@ -2,50 +2,71 @@
 
 import { Cpf } from "../campos/cpf.js";
 import { Data } from "../campos/data.js";
+import { Horario } from "../campos/horario.js";
 import { IntervaloDeHorario } from "../campos/intervalo-de-horario.js";
 
 export class Agendamento {
+  #cpfDoPaciente;
+  #dia;
+  #intervaloDeHorario;
 
-    #cpfDoPaciente;
-    #dia;
-    #intervaloDeHorario;
+  get cpfDoPaciente() {
+    return this.#cpfDoPaciente;
+  }
 
-    get cpfDoPaciente() {
-        return this.#cpfDoPaciente
+  get dia() {
+    return this.#dia;
+  }
+
+  get intervaloDeHorario() {
+    return this.#intervaloDeHorario;
+  }
+
+  /**
+   *
+   * @param {Cpf} cpfDoPaciente
+   * @param {Data} dia
+   * @param {IntervaloDeHorario} intervaloDeHorario
+   */
+  constructor(cpfDoPaciente, dia, intervaloDeHorario) {
+    Agendamento.#validaIntervaloDeHorario(intervaloDeHorario);
+
+    this.#cpfDoPaciente = cpfDoPaciente;
+    this.#dia = dia;
+    this.#intervaloDeHorario = intervaloDeHorario;
+  }
+
+  /**
+   * @param {IntervaloDeHorario} intervaloDeHorario
+   */
+  static #validaIntervaloDeHorario(intervaloDeHorario) {
+    const minutosValidos = [0, 15, 30, 45];
+    const minutoInicio = intervaloDeHorario.inicio.minuto;
+    const minutoFim = intervaloDeHorario.fim.minuto;
+    if (
+      !minutosValidos.includes(minutoInicio) ||
+      !minutosValidos.includes(minutoFim)
+    ) {
+      throw new Error(
+        "O Intervalo de Horario do Agendamento deve ser definido em termos de 15 em 15 minutos"
+      );
+    }
+  }
+
+  estaNoPassado() {
+    if (this.dia < Data.hoje()) {
+      // é de um dia antes de hoje
+      return true;
     }
 
-    get dia() {
-        return this.#dia;
+    if (
+      this.dia.equals(Data.hoje()) &&
+      this.intervaloDeHorario.inicio < Horario.agora()
+    ) {
+      // é de hoje, mas de um horário que já passou
+      return true;
     }
 
-    get intervaloDeHorario() {
-        return this.#intervaloDeHorario;
-    }
-
-    /**
-     * 
-     * @param {Cpf} cpfDoPaciente
-     * @param {Data} dia 
-     * @param {IntervaloDeHorario} intervaloDeHorario
-     */
-    constructor(cpfDoPaciente, dia, intervaloDeHorario) {
-        Agendamento.#validaIntervaloDeHorario(intervaloDeHorario);
-
-
-        this.#cpfDoPaciente = cpfDoPaciente;
-        this.#dia = dia;
-        this.#intervaloDeHorario = intervaloDeHorario;
-    }
-
-    /**
-     * @param {IntervaloDeHorario} intervaloDeHorario 
-    */
-    static #validaIntervaloDeHorario(intervaloDeHorario) {
-        const minutosValidos = [0, 15, 30, 45];
-        const minutoInicio = intervaloDeHorario.inicio.minuto;
-        const minutoFim = intervaloDeHorario.fim.minuto;
-        if (!minutosValidos.includes(minutoInicio) || !minutosValidos.includes(minutoFim)) {
-            throw new Error('O Intervalo de Horario do Agendamento deve ser definido em termos de 15 em 15 minutos');
-        }
-    }
+    return false;
+  }
 }
---
+++ src/dominio/use-cases/cancelar-agendamento.js
@@ -0,0 +1,46 @@
+// @ts-check
+
+import { Cpf } from "../campos/cpf";
+import { Data } from "../campos/data";
+import { Horario } from "../campos/horario";
+import { Agendamento } from "../entidades/agendamento";
+import { AgendamentoRepository } from "../repository/agendamento.repository";
+
+export class CancelarAgendamentoUseCase {
+  #agendamentoRepository;
+
+  /**
+   *
+   * @param {AgendamentoRepository} agendamentoRepository
+   */
+  constructor(agendamentoRepository) {
+    this.#agendamentoRepository = agendamentoRepository;
+  }
+
+  /**
+   *
+   * @param {Cpf} cpf
+   * @param {Data} dia
+   * @param {Horario} horaInicial
+   * @returns {Promise<Agendamento>} agendamento cancelado
+   */
+  async executar(cpf, dia, horaInicial) {
+    const agendamento = await this.#agendamentoRepository.buscar(
+      cpf,
+      dia,
+      horaInicial
+    );
+
+    if (!agendamento) {
+      throw new Error("Agendamento não encontrado");
+    }
+
+    if (agendamento.estaNoPassado()) {
+      throw new Error("Não é possível cancelar um agendamento passado");
+    }
+
+    await this.#agendamentoRepository.excluir(agendamento);
+
+    return agendamento;
+  }
+}
