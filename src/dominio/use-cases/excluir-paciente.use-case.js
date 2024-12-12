e1e3c85c092e5ce468103266b04b98eedfe44818
Rodrigo de França <rodrigodefrancadev@gmail.com>
2024-12-12 14:50:38 -0300

    excluir paciente usecase

---
+++ src/dominio/use-cases/excluir-paciente.use-case.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+import { Cpf } from "../campos/cpf.js";
+import { AgendamentoRepository } from "../repository/agendamento.repository.js";
+import { PacienteRepository } from "../repository/paciente.repository.js";
+
+export class ExcluiirPacienteUseCase {
+  #pacienteRepository;
+  #agendamentoRepository;
+
+  /**
+   *
+   * @param {PacienteRepository} pacienteRepository
+   * @param {AgendamentoRepository} agendamentoRepository
+   */
+  constructor(pacienteRepository, agendamentoRepository) {
+    this.#pacienteRepository = pacienteRepository;
+    this.#agendamentoRepository = agendamentoRepository;
+  }
+
+  /**
+   *
+   * @param {Cpf} cpf
+   */
+  async executar(cpf) {
+    const paciente = await this.#pacienteRepository.buscarPorCpf(cpf);
+    if (!paciente) {
+      throw new Error("Paciente não encontrado");
+    }
+
+    const agendamentos = await this.#agendamentoRepository.filtrar({ cpf });
+
+    const temAgendamentoFuturo = agendamentos.some((ag) => ag.estaNoFuturo());
+
+    if (temAgendamentoFuturo) {
+      throw new Error(
+        "Não é permitido excluir paciente que possui agendamentos futuros"
+      );
+    }
+
+    await this.#agendamentoRepository.excluirVarios(agendamentos);
+    await this.#pacienteRepository.exluir(paciente);
+  }
+}
